2d0989697cb7cb59341a10bc1fb6f2b4747090f0
saumitrajain <saumitrajain@gmail.com>
2019-03-10 15:09:13 -0500

    Update fiboanci.py

--- fiboanci.py
+++ fiboanci.py
@@ -1,18 +1,14 @@
 
 def fib(i):
     sum = 0
-    for n in range(1,i):
-        if n<=1:
-            sum=1
-        if n%2==0:
-            
-            if n<=1:
-                return 1
-            if n==2:
-                return 2
-            if n>2:
-               sum= fib(n-1)+fib(n-2)  
-               
+    x=0
+    y=1        
+    if n<=1:
+        return 1
+    if n==2:
+        return 2
+    if n>2:
+        sum= fib(n-1)+fib(n-2)               
                
     print sum         
     
